7f10b02acc1da606226113926f24499a91925349
Pepe Alegre <jalegrea@gmail.com>
2017-09-03 01:49:20 -0500

    Initial checkin of Sales Tool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,19 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data= new SalesData();
+		
+		displayGretting();
+		data.display();
+
+	}
+
+	private static void displayGretting() {
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This App shows sales data");
+		
+	}
+
+}
